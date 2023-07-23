d0e4f67f3a7fe13694773781727528909773f43d
Ryan <rruckley@gmail.com>
2023-07-23 11:31:50 +1000

    new config mod.

---
+++ src/config.rs
@@ -0,0 +1,28 @@
+//! Config data management
+//! 
+
+use std::env;
+
+struct Config {}
+
+impl Config {
+    /// Get a single configuration first from ENV then falling back to hard coded defaults
+    pub fn get(item : & str) -> Option<String> {
+        match env::var(item) {
+            Ok(i) => Some(i),
+            _ => Config::get_default(item),
+        }
+    }
+
+    /// Return hard coded default configuration items
+    fn get_default(item : &str) -> Option<String> {
+        match item {
+            "VERSION" => {
+                let name = env!("CARGO_PKG_NAME");
+                let vers = env!("CARGO_PKG_VERSION");
+                Some(format!("{}/{}",name,vers))
+            },
+            _ => None,
+        }
+    }
+}
