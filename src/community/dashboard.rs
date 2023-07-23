278959801343716ab989e9d2256a517b30537b0c
Ryan <rruckley@gmail.com>
2023-07-23 12:16:12 +1000

    simple implementation

--- src/community/dashboard.rs
+++ src/community/dashboard.rs
@@ -1,5 +1,39 @@
 //! Dashboard Module
 //! 
 
-/// Dashboard Structure
+/// Dashboard Model
+pub struct DashboardModel {
+    id : Option<String>,
+    uid : Option<String>,
+    title : String,
+    tags : Option<Vec<String>>,
+    timezone : String,
+    schemaVersion : u16,
+    refresh : String,
+}
+/// Data for creating a new dashboard
+pub struct CreateDashboard {
+    dashboard : DashboardModel,
+    folderId : u16,
+    folderUid: Option<String>,
+    message : String,
+    overwrite : bool,
+}
+
+/// Dashboard Error
+pub struct DashboardError {
+    pub message : String,
+    pub status : String,
+}
+
+/// Dashboard API Structure
 pub struct Dashboard {}
+
+impl Dashboard {
+    pub fn create(create : CreateDashboard) -> Result<DashboardModel,DashboardError> {
+        Err(DashboardError {
+            message : "Not implemented".to_string(),
+            status : "ERROR".to_string(),
+        })
+    }
+}
