f74507ebb9723f47d294d6d5aa38d97fc5f23f19
Ryan <rruckley@gmail.com>
2023-07-24 21:45:23 +1000

    Doc Comments

--- src/client.rs
+++ src/client.rs
@@ -1,23 +1,33 @@
 //! Grafana Client 
 //! 
 
-use community::*;
-use config;
+
+use crate::config::Config;
+use crate::community::admin::Admin;
+use crate::community::annotations::Annotations;
+use crate::community::alerting_provisioning::AlertingProvisioning;
 
 /// Client Structure
 pub struct Client {
-    // Commoon
+    /// Common configuration information
     pub config : Config,
-    // Community
+    /// Admin API
     pub admin : Admin,
+    /// Annoations API
     pub annotations : Annotations,
+    /// Alert Provsioning API
     pub alerting_provisioning : AlertingProvisioning,
 }
 
 
 impl Client {
-    pub fn new() -> Client {
+    /// Create a new client instance
+    pub fn new(url : String) -> Client {
         Client {
+            config : Config::new(url),
+            admin : Admin {},
+            annotations : Annotations {  },
+            alerting_provisioning : AlertingProvisioning {  },
 
         }
     }
