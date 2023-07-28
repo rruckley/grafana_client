32237ed17cb7e4f5c73e9ec981796262cb847fe2
Ryan <rruckley@gmail.com>
2023-07-28 15:59:56 +1000

    moved client

---
+++ grafana-lib/src/client.rs
@@ -0,0 +1,60 @@
+//! Grafana Client
+//! 
+use crate::config::Config;
+use crate::community::admin::Admin;
+use crate::community::annotations::Annotations;
+use crate::community::alerting_provisioning::AlertingProvisioning;
+use crate::community::authentication::Authentication;
+use crate::community::dashboard::Dashboard;
+
+use serde::Serialize;
+use reqwest::*;
+
+/// Client Structure
+pub struct Client {
+    /// Common configuration information
+    pub config : Config,
+    /// Admin API
+    pub admin : Admin,
+    /// Annoations API
+    pub annotations : Annotations,
+    /// Alert Provsioning API
+    pub alerting_provisioning : AlertingProvisioning,
+    /// Authentication API
+    pub authentication : Authentication,
+    /// Dashboard API
+    pub dashboard : Option<Dashboard>,
+}
+
+
+impl Client {
+    /// Create a new client instance
+    pub fn new(url : String) -> Client {
+        Client {
+            config : Config::new(url),
+            admin : Admin {},
+            annotations : Annotations {  },
+            alerting_provisioning : AlertingProvisioning {  },
+            authentication : Authentication {  },
+            dashboard : None,
+        }
+    }
+
+    /// Send compatible struct through to Grafana
+    pub async fn send<T>(&self, payload : T) -> Result<String> 
+    where T : Sized + Serialize,
+    {
+        let client = reqwest::Client::new();
+        match client.post(self.config.url())
+            .json(&payload)
+            .send().await {
+                Ok(_) => Ok("Yay".to_string()),
+                Err(e) => Err(e),
+            }
+    }
+
+    /// Connect to Grafana
+    pub fn connect() -> Result<String> {
+        Ok(String::from("Wow!"))
+    }
+}
