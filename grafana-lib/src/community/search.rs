f85a42c05d58b4a5e25670289429e01691c4d702
Ryan <rruckley@gmail.com>
2023-07-28 17:16:54 +1000

    basic search fn

--- grafana-lib/src/community/search.rs
+++ grafana-lib/src/community/search.rs
@@ -1,4 +1,27 @@
 //! Search Module
 
+use crate::error::GrafanaError;
+
+/// Dashboard Search Results
+pub struct DashboardResult {
+    id  : String,
+}
+
+/// Folder Search Results
+pub struct FolderResult {
+    id  : String,
+}
+
 /// Search Structure
 pub struct Search {}
+
+impl Search {
+    /// Search dashboards according to query string
+    pub fn dashboard(query : Option<String>) -> Result<Vec<DashboardResult>,GrafanaError> {
+        Err(GrafanaError { message: String::from("Not implemented"), status: String::from("-1") })
+    }
+
+    pub fn folder(query : Option<String>) -> Result<Vec<FolderResult>,GrafanaError> {
+        Err(GrafanaError { message: String::from("Not implemented"), status: String::from("-1") })
+    }
+}
