dc4d037ea3d73523485ff5282377275babd1bec8
Ryan <rruckley@gmail.com>
2023-07-28 16:00:06 +1000

    moved to here

---
+++ grafana-lib/src/error.rs
@@ -0,0 +1,11 @@
+//! Error Module
+//! 
+//! 
+
+/// Error Structure
+pub struct GrafanaError {
+    /// Error Message
+    pub message : String,
+    /// Error Status Code
+    pub status : String,
+}
---
+++ grafana-lib/src/lib.rs
@@ -0,0 +1,20 @@
+
+//! Client library for Grafana
+//! 
+//! Provides a simple library to use for interacting with Grafana 10+
+//! 
+//! 
+
+#![warn(missing_docs)]
+
+/// Common modules
+pub mod config;
+pub mod client;
+pub mod error;
+
+/// Community modules, included by default
+pub mod community;
+
+/// Enterprise modules
+#[cfg(feature = "enterprise")]
+pub mod enterprise;
