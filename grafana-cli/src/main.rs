8ebe84b8fb91c16b908534023a0f3ae51ea900cf
Ryan <rruckley@gmail.com>
2023-07-28 17:17:20 +1000

    expanded main fn

--- grafana-cli/src/main.rs
+++ grafana-cli/src/main.rs
@@ -1,9 +1,53 @@
 //! Grafana CLI using the Grafana LIB crate
 //! 
 //! 
-use grafana_lib::client::Client;
+use grafana_lib::client::{Client};
+use clap::{Parser,Subcommand};
+use log::info;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Optional host, overrides GRAFANA_HOST environment 
+    #[arg(long)]
+    host: Option<String>,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+// CLI commands
+#[derive(Subcommand,Debug)]
+pub enum Commands {
+    Dashboard {
+        #[arg(short, long)]
+        list: bool,
+    },
+    Folder {
+        #[arg(short, long)]
+        list: bool,
+    }
+}
 
 fn main() {
+    let args = Args::parse();
+
     // Create a client to use for cli
-    let _client = Client::new(String::from("http://localhost:3000"));
+    let client = Client::new(String::from("http://localhost:3000"));
+
+    dbg!(&args.command);
+    match args.command {
+        Commands::Dashboard { list } => {
+            info!("Executing Dashboard");
+            if list {
+                let results = client.search_dashboards(None);
+            }
+        },
+        Commands::Folder { list } => {
+            info!("Executing Folder Search");
+            if list {
+                let results = client.search_folders(None);
+            }
+        }
+    }
 }
