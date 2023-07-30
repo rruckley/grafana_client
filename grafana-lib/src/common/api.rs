b011466647a6e26a9292c03f2029f14860aa3d48
Ryan <rruckley@gmail.com>
2023-07-30 18:40:11 +1000

    constructor

--- grafana-lib/src/common/api.rs
+++ grafana-lib/src/common/api.rs
@@ -1,26 +1,36 @@
 //! API Module
 //! 
 
+use serde::Serialize;
 
 /// Low level API functions
-struct Api {}
+pub struct Api {
+    token   : String,
+    host    : String,
+}
 
 impl Api {
-    async fn get(&self, url : String) -> Result<String> {
-        let _body = reqwest::get(url).await?;
+    pub fn new(host : String,token : String) -> Api {
+        Api {
+            token,
+            host,
+        }
+    }
+    async fn get(&self, url : String) -> Result<String,String> {
+        let _body = reqwest::get(url).await.unwrap();
         Ok(String::from("It was good"))
     }
 
     /// Send compatible struct through to Grafana
-    async fn post<T>(&self, payload : T) -> Result<String> 
+    async fn post<T>(self, payload : T) -> Result<String,String> 
     where T : Sized + Serialize,
     {
         let client = reqwest::Client::new();
-        match client.post(self.config.url())
+        match client.post(self.host)
             .json(&payload)
             .send().await {
                 Ok(_) => Ok("Yay".to_string()),
-                Err(e) => Err(e),
+                Err(e) => Err(e.to_string()),
             }
     }
 }
