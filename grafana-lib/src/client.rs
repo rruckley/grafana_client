e48bf154127c7f5fc4f1eccc5e8c7b3ef95b7b42
Ryan <rruckley@gmail.com>
2023-07-30 15:51:49 +1000

    move fn to api mod

--- grafana-lib/src/client.rs
+++ grafana-lib/src/client.rs
@@ -79,24 +79,6 @@ impl Client {
         }
     }
 
-    async fn get(&self, url : String) -> Result<String> {
-        let _body = reqwest::get(url).await?;
-        Ok(String::from("It was good"))
-    }
-
-    /// Send compatible struct through to Grafana
-    async fn post<T>(&self, payload : T) -> Result<String> 
-    where T : Sized + Serialize,
-    {
-        let client = reqwest::Client::new();
-        match client.post(self.config.url())
-            .json(&payload)
-            .send().await {
-                Ok(_) => Ok("Yay".to_string()),
-                Err(e) => Err(e),
-            }
-    }
-
     /// Connect to Grafana
     pub fn connect() -> Result<String> {
         Ok(String::from("Wow!"))
