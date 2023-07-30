46f9ed0db1b732ff977bd9116fd169009e3f8940
Ryan <rruckley@gmail.com>
2023-07-30 15:51:41 +1000

    move to api mod

--- grafana-lib/src/common/api.rs
+++ grafana-lib/src/common/api.rs
@@ -6,7 +6,21 @@
 struct Api {}
 
 impl Api {
-    pub fn get(url : String) -> Result<String,String> {
-        Err(String::from("API::get() - Not implemented"))
+    async fn get(&self, url : String) -> Result<String> {
+        let _body = reqwest::get(url).await?;
+        Ok(String::from("It was good"))
+    }
+
+    /// Send compatible struct through to Grafana
+    async fn post<T>(&self, payload : T) -> Result<String> 
+    where T : Sized + Serialize,
+    {
+        let client = reqwest::Client::new();
+        match client.post(self.config.url())
+            .json(&payload)
+            .send().await {
+                Ok(_) => Ok("Yay".to_string()),
+                Err(e) => Err(e),
+            }
     }
 }
