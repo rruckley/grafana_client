62e05d0c84d5c3719b5928355796291d1027b220
Ryan Ruckley <rruckley@gmail.com>
2023-08-03 09:20:04 +1000

    suppress err

--- grafana-lib/src/community/search.rs
+++ grafana-lib/src/community/search.rs
@@ -5,12 +5,12 @@ use crate::common::api::Api;
 
 /// Dashboard Search Results
 pub struct DashboardResult {
-    id  : String,
+    _id  : String,
 }
 
 /// Folder Search Results
 pub struct FolderResult {
-    id  : String,
+    _id  : String,
 }
 
 /// Search Structure
@@ -29,14 +29,14 @@ impl Search {
     /// Search dashboards according to query string
     pub fn dashboard(&self, query : Option<String>) -> Result<Vec<DashboardResult>,GrafanaError> {
         let url = format!("{}?{}",self.api.host,query.unwrap());
-        let json = self.api.get(url);
+        let _json = self.api.get(url);
         Err(GrafanaError { message: String::from("Dashboard Search: Not implemented"), status: String::from("-1") })
     }
 
     /// Folder Search using query string
     pub fn folder(&self, query : Option<String>) -> Result<Vec<FolderResult>,GrafanaError> {
         let url = format!("{}?{}",self.api.host,query.unwrap());
-        let json = self.api.get(url);
+        let _json = self.api.get(url);
         Err(GrafanaError { message: String::from("Folder Search: Not implemented"), status: String::from("-1") })
     }
 }
