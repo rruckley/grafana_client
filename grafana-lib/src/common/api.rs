7b7d4072f2de6df3b82e210f2ba6a5cb92f0ec18
Ryan Ruckley <rruckley@gmail.com>
2023-08-03 13:46:47 +1000

    blocking git

--- grafana-lib/src/common/api.rs
+++ grafana-lib/src/common/api.rs
@@ -3,6 +3,8 @@
 
 use serde::Serialize;
 
+use log::debug;
+
 /// Low level API functions
 pub struct Api {
     token   : String,
@@ -18,14 +20,28 @@ impl Api {
             host : format!("{}/api",host),
         }
     }
-    /// Perform GET operation against Grafana
-    pub async fn get(&self, url : String) -> Result<String,String> {
+    /// Perform GET operation against Grafana using blocking
+    pub fn get(&self, path : String) -> Result<String,String> {
+        let url = format!("{}/{}",self.host, path);
+        match reqwest::blocking::get(url) {
+            Ok(r) => {
+                Ok(r.json().unwrap())
+            },
+            Err(e) => {
+                Err(e.to_string())
+            }
+        }
+    }
+    /// Perform GET operation against Grafana using async
+    pub async fn get_async(&self, path : String) -> Result<String,String> {
+        let url = format!("{}/{}",self.host,path);
+        debug!("URL: {url}");
         let _body = reqwest::get(url).await.unwrap();
         Ok(String::from("It was good"))
     }
 
-    /// Send compatible struct through to Grafana
-    pub async fn post<T>(self, payload : T) -> Result<String,String> 
+    /// Send compatible struct through to Grafana using async
+    pub async fn post_async<T>(self, payload : T) -> Result<String,String> 
     where T : Sized + Serialize,
     {
         let client = reqwest::Client::new();
