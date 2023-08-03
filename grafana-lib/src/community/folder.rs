bdf1ad1edde1b985fb477b8faeabbd359bf52f84
Ryan Ruckley <rruckley@gmail.com>
2023-08-03 11:47:29 +1000

    fns for create folder

--- grafana-lib/src/community/folder.rs
+++ grafana-lib/src/community/folder.rs
@@ -1,13 +1,33 @@
 //! Folder Module
 //! 
+use log::info;
 
 /// Data model for a folder
 pub struct FolderModel {
+    /// Folder name
     pub name : String,
 }
 
 /// Folder Struct
-pub struct Folder {}
+pub struct Folder {
+    model : Option<FolderModel>,
+}
 
 impl Folder {
+    /// Create empty instance of folder API
+    pub fn new() -> Folder {
+        Folder {
+            model : None,
+        }
+    }
+    /// Create new folder 
+    pub fn create(mut self, model : FolderModel) -> Folder {
+        info!("Creating new dashboard: {}",model.name);
+        self.model = Some(model);
+        self
+    }
+    /// Send folder to Grafana
+    pub fn send(&self) -> Result<String,String> {
+        Err(String::from("Create Folder: Not implemented"))
+    }
 }
