4af397839798aa93c417aec27c7cc3d5d2685242
Ryan Ruckley <rruckley@gmail.com>
2023-08-03 11:47:02 +1000

    generate url

--- grafana-lib/src/community/search.rs
+++ grafana-lib/src/community/search.rs
@@ -3,6 +3,12 @@
 use crate::common::error::GrafanaError;
 use crate::common::api::Api;
 
+use log::debug;
+
+const SEARCH_PATH       : &str = "search";
+const SEARCH_DASHBOARD  : &str = "dash-db";
+const SEARCH_FOLDER     : &str = "dash-folder";
+
 /// Dashboard Search Results
 pub struct DashboardResult {
     _id  : String,
@@ -21,21 +27,29 @@ pub struct Search {
 impl Search {
     /// Create a new search instance
     pub fn new(host : String, token : String) -> Search {
-        let api = Api::new(token,host);
+        let api = Api::new(host,token);
         Search {
             api,
         }
     }
     /// Search dashboards according to query string
     pub fn dashboard(&self, query : Option<String>) -> Result<Vec<DashboardResult>,GrafanaError> {
-        let url = format!("{}?{}",self.api.host,query.unwrap());
+        let url = match query {
+            Some(q) => format!("{}/{}?type={}&query={}",self.api.host,SEARCH_PATH,SEARCH_DASHBOARD,q),
+            None    => format!("{}/{}?type={}",self.api.host,SEARCH_PATH,SEARCH_DASHBOARD),
+        };
+        debug!("URL: {url}");
         let _json = self.api.get(url);
         Err(GrafanaError { message: String::from("Dashboard Search: Not implemented"), status: String::from("-1") })
     }
 
     /// Folder Search using query string
     pub fn folder(&self, query : Option<String>) -> Result<Vec<FolderResult>,GrafanaError> {
-        let url = format!("{}?{}",self.api.host,query.unwrap());
+        let url = match query {
+            Some(q) => format!("{}/{}?type={}&query={}",self.api.host,SEARCH_PATH,SEARCH_FOLDER,q),
+            None    => format!("{}/{}?type={}",self.api.host,SEARCH_PATH,SEARCH_FOLDER),
+        };
+        debug!("URL: {url}");
         let _json = self.api.get(url);
         Err(GrafanaError { message: String::from("Folder Search: Not implemented"), status: String::from("-1") })
     }
