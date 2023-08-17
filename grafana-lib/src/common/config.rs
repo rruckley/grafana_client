541a507013cc6ce5d3d02bed897639ffacf841f2
Ryan Ruckley <rruckley@gmail.com>
2023-08-17 16:42:56 +1000

    add token in builder style

--- grafana-lib/src/common/config.rs
+++ grafana-lib/src/common/config.rs
@@ -4,26 +4,40 @@
 use std::env;
 
 /// Handles configuration data
+#[derive(Debug)]
 pub struct Config {
     /// Private field
     host : String,
+    token : Option<String>,
 }
 
 impl Config {
     /// Create a new configuration instance
     pub fn new(host : String) -> Config {
         Config {
-            host
+            host,
+            token : None,
         }
     }
+
+    /// Add token into config object
+    pub fn with_token(mut self, token : String) -> Config {
+        self.token = Some(token);
+        self
+    }
+
     /// Return URL for connecting to Grafana
     pub fn url(&self) -> String {
         format!("https://{}",self.host).to_string()
     }
+
     /// Get a single configuration first from ENV then falling back to hard coded defaults
     pub fn get(&self, item : & str) -> Option<String> {
         if item == "GRAFANA_HOST" {
             return Some(self.host.clone());
+        }
+        if item == "GRAFANA_TOKEN" {
+            return self.token.clone()
         }
         match Config::get_env(item) {
             Some(i) => Some(i),
